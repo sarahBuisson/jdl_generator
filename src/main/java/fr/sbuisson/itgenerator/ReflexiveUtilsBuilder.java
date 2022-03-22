754c785761a854342d7bfef1a49587a1d9826fa7
sarah <sarah.buisson@gmail.com>
2022-03-23 00:17:58 +0100

    add reflexive json set

---
+++ src/main/java/fr/sbuisson/itgenerator/ReflexiveUtilsBuilder.java
@@ -0,0 +1,106 @@
+package fr.sbuisson.itgenerator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.*;
+import fr.sbuisson.jdlgenerator.Utils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static com.github.javaparser.utils.Utils.capitalize;
+
+public class ReflexiveUtilsBuilder {
+
+    public String mainScript = "";
+    public Map<String, String> otherMethodes = new HashMap<>();
+    public int minLength = 5;
+
+    public ReflexiveUtilsBuilder(String json, String nodeName) {
+
+        this(json, nodeName, 5);
+
+    }
+
+    public ReflexiveUtilsBuilder(String json, String nodeName, int minLength) {
+        this.minLength = minLength;
+        try {
+            var node = new ObjectMapper().readTree(json);
+            mainScript =setBuilderFromJson(node, nodeName, capitalize(nodeName));
+
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String setFromJson(JsonNode node, String nodeName) {
+
+
+        if (node instanceof IntNode) {
+            return node.asText();
+        } else if (node instanceof TextNode) {
+            return "\"" + node.asText() + "\"";
+        } else if (node instanceof ArrayNode) {
+            final StringBuilder builder = new StringBuilder();
+            builder.append("var " + nodeName + " = new ArrayList<>();\n");
+            AtomicReference<Integer> index = new AtomicReference<>(0);
+            node.elements().forEachRemaining((nodeArray) -> {
+                if (nodeArray instanceof ObjectNode) {
+                    builder.append("var " + nodeName + index.get() + " = build" + Utils.capitalize(nodeName) + "();\n");
+
+                    setBuilderFromJson(nodeArray, nodeName + index.get(), Utils.capitalize(nodeName));
+                    builder.append(nodeName + ".add(" + nodeName + index.get() + ");\n");
+                    index.set(index.get() + 1);
+
+                } else {
+                    builder.append(nodeName + ".add(" + nodeArray.asText() + ");\n");
+                }
+
+            });
+
+            return builder.toString();
+
+        } else if (node instanceof ObjectNode) {
+
+            return setBuilderFromJson(node, nodeName, capitalize(nodeName));
+
+        }
+        return "";
+
+    }
+
+    private String setBuilderFromJson(JsonNode node, String nodeName, String className) {
+        String methodName = "build" +className;
+        if (!otherMethodes.containsKey(className)) {
+            String script = "";
+
+            for (Iterator<Map.Entry<String, JsonNode>> i = node.fields(); i.hasNext(); ) {
+                var childNode = i.next();
+                if (childNode.getValue() instanceof IntNode || node instanceof FloatNode || node instanceof LongNode || node instanceof DoubleNode || node instanceof BooleanNode) {
+                    script += nodeName + ".set" + capitalize((childNode.getKey())) + "(" + childNode.getValue().asText() + ");\n";
+                } else if (childNode.getValue() instanceof TextNode) {
+                    script += nodeName + ".set" + capitalize((childNode.getKey())) + "(\"" + childNode.getValue().asText() + "\");\n";
+                } else {
+                    script += (setFromJson(childNode.getValue(), childNode.getKey()));
+                    script += nodeName + ".set" + capitalize((childNode.getKey())) + "(" + childNode.getKey() + ");\n";
+                }
+
+            }
+            if (node.size() > minLength) {
+                return "var " + nodeName + " = new " + className + "();\n" + script;
+            } else {
+                script =  "var " + nodeName + " = new " + className + "();\n" + script;
+                otherMethodes.put(methodName, script);
+                return "var " + nodeName + " = " + methodName + "();\n";
+            }
+        }
+        return "";
+    }
+
+}
---
+++ src/test/java/fr/sbuisson/itgenerator/ReflexiveUtilsBuilderTest.java
@@ -0,0 +1,93 @@
+package fr.sbuisson.itgenerator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+
+class ReflexiveUtilsBuilderTest {
+
+    @Test
+    public void testSimpleJson() {
+
+        var actual = new ReflexiveUtilsBuilder("{\"a\":\"b\",\"c\":\"d\"}", "request");
+        Assertions.assertEquals(1, actual.otherMethodes.size());
+        Assertions.assertEquals("var request = buildRequest();\n", actual.mainScript);
+        Assertions.assertEquals("var request = new Request();\n" +
+                "request.setA(\"b\");\nrequest.setC(\"d\");\n", actual.otherMethodes.get("buildRequest"));
+
+
+    }
+
+   @Test
+    public void testComplexeJson() {
+
+        var actual = new ReflexiveUtilsBuilder("{\"a\":\"b\",\"c\":{\"d\":4}, \"e\":[4,5], \"f\":[{\"g\":\"h\"}]}", "request");
+        Assertions.assertEquals(3, actual.otherMethodes.size());
+        Assertions.assertEquals("var request = buildRequest();\n", actual.mainScript);
+        Assertions.assertEquals("var request = new Request();\n" +
+                "request.setA(\"b\");\n" +
+                "var c = buildC();\n" +
+                "request.setC(c);\n" +
+                "var e = new ArrayList<>();\n" +
+                "e.add(4);\n" +
+                "e.add(5);\n" +
+                "request.setE(e);\n" +
+                "var f = new ArrayList<>();\n" +
+                "var f0 = buildF();\n" +
+                "f.add(f0);\n" +
+                "request.setF(f);\n", actual.otherMethodes.get("buildRequest"));
+
+
+    }
+
+    @Test
+    public void testSimple2Json() {
+
+        var actual = new ReflexiveUtilsBuilder("{\"a\":{\"b\":2}}", "request", 1);
+        Assertions.assertEquals(2, actual.otherMethodes.size());
+
+        Assertions.assertEquals("var request = buildRequest();\n", actual.mainScript);
+
+        Assertions.assertEquals("var request = new Request();\n" +
+                "var a = buildA();\n" +
+                "request.setA(a);\n", actual.otherMethodes.get("buildRequest"));
+
+        Assertions.assertEquals("var a = new A();\n" +
+                "a.setB(2);\n", actual.otherMethodes.get("buildA"));
+
+    }
+
+
+    @Test
+    public void testSimpleArrayJson() {
+
+        var actual = new ReflexiveUtilsBuilder("{\"a\":[{\"b\":2},{\"b\":3}]}", "request", 1);
+
+
+        Assertions.assertEquals("var request = buildRequest();\n", actual.mainScript);
+
+        Assertions.assertEquals("var request = new Request();\n" +
+                "var a = new ArrayList<>();\n" +
+                "var a0 = buildA();\n" +
+                "a.add(a0);\n" +
+                "var a1 = buildA();\n" +
+                "a.add(a1);\n" +
+                "request.setA(a);\n",
+                actual.otherMethodes.get("buildRequest"));
+
+        Assertions.assertEquals("var a1 = new A();\n" +
+                "a1.setB(3);\n", actual.otherMethodes.get("buildA"));
+        Assertions.assertEquals(2, actual.otherMethodes.size());
+
+    }
+
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
